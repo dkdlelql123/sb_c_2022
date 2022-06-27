4da8e7d5358cb0649b517dfa3e6f9bb8344e1e4f
NamYeJi <dkdlelql123@naver.com>
2022-06-27 11:41:19 +0900

    UsrArticleController 서비스 로직 ArticleService로 옮김

--- src/main/java/com/nyj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/nyj/exam/demo/controller/UsrArticleController.java
@@ -1,68 +1,39 @@
 package com.nyj.exam.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.nyj.exam.demo.service.ArticleService;
 import com.nyj.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
 	
-	private int articleLastId;
-	private List<Article> articles;
-	
-	public UsrArticleController() {
-		articleLastId = 0;
-		articles = new ArrayList<Article>() ;
-	}
-
-	private Article getArticle(int id) {
-		for(Article article : articles) {
-			if( article.getId() == id) {
-				return article; 
-			}
-		}
-		return null;
-	}
-	
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-	}
-	 
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		article.setTitle(title);
-		article.setBody(body); 
-	}
-	
-	
+	@Autowired
+	ArticleService articleService;
 	
 	
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		articleLastId++;
-		Article article = new Article(articleLastId, title, body);
-		articles.add(article);
-		
+		Article article = articleService.writeArticle(title, body);
 		return article;
 	}
-	
+
 	@RequestMapping("/usr/article/showList")
 	@ResponseBody
 	public List<Article> showList() {	
-		return articles;
+		return articleService.getArticles();
 	}
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
 	public Object getArticleAction(int id) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id+"번 게시물이 존재하지 않습니다.";
@@ -74,13 +45,13 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id+"번 게시물이 존재하지 않습니다.";
 		}
 		
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 		
 		return id+"번 게시물이 삭제되었습니다.";
 	}
@@ -88,13 +59,13 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id+"번 게시물이 존재하지 않습니다.";
 		}
 		
-		modifyArticle(id,title,body);
+		articleService.modifyArticle(id,title,body);
 		return id+"번 게시물이 수정되었습니다.";
 	}
 
---
+++ src/main/java/com/nyj/exam/demo/service/ArticleService.java
@@ -0,0 +1,54 @@
+package com.nyj.exam.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nyj.exam.demo.vo.Article;
+
+@Service
+public class ArticleService {
+
+	private int articleLastId;
+	private List<Article> articles;
+	
+	public ArticleService() {
+		articleLastId = 0;
+		articles = new ArrayList<Article>();
+	}
+	
+	public Article writeArticle(String title, String body) {
+		articleLastId++;
+		Article article = new Article(articleLastId, title, body);
+		articles.add(article);
+		return article;
+	}
+
+	public Article getArticle(int id) {
+		for(Article article : articles) {
+			if( article.getId() == id) {
+				return article; 
+			}
+		}
+		return null;
+	}
+	
+	public List<Article> getArticles() { 
+		return articles;
+	}
+	
+	public void deleteArticle(int id) {
+		Article article = getArticle(id);
+		articles.remove(article);
+	}
+	 
+	public void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		article.setTitle(title);
+		article.setBody(body); 
+	}
+	
+	
+
+}
