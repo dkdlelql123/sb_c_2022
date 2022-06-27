49b0f772a53a2d3bfa806f3a0396dd39821b0ad7
NamYeJi <dkdlelql123@naver.com>
2022-06-27 14:11:03 +0900

    INSERT 쿼리 메세드와 연결, last_insert_id 사용

--- src/main/java/com/nyj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/nyj/exam/demo/repository/ArticleRepository.java
@@ -14,8 +14,8 @@ import com.nyj.exam.demo.vo.Article;
 @Mapper
 public interface ArticleRepository {  
 	
-	//@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title =  #{title},`body` =  #{body}")
-	public Article writeArticle(@Param("title") String title, @Param("body") String body);  
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title =  #{title},`body` =  #{body}")
+	public void writeArticle(@Param("title") String title, @Param("body") String body);  
 
 	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id); 
@@ -28,5 +28,8 @@ public interface ArticleRepository {
 	 
 	@Update("UPDATE article SET title = #{title}, `body`= #{body}, updateDate = NOW() WHERE id =  #{id}")
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 	
 }
