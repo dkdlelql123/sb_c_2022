ab60be8323429edeac99de8d3589ee5d3eed2114
NamYeJi <dkdlelql123@naver.com>
2022-06-27 11:14:07 +0900

    게시물 수정 액션

--- src/main/java/com/nyj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/nyj/exam/demo/controller/UsrArticleController.java
@@ -29,6 +29,20 @@ public class UsrArticleController {
 		return null;
 	}
 	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		articles.remove(article);
+	}
+	 
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		article.setTitle(title);
+		article.setBody(body); 
+	}
+	
+	
+	
+	
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -45,16 +59,31 @@ public class UsrArticleController {
 		return articles;
 	}
 	
-	@RequestMapping("/usr/article/delete")
+	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String deleteArticle(int id) {
+	public String doDelete(int id) {
 		Article article = getArticle(id);
 		
 		if(article == null) {
 			return id+"번 게시물이 존재하지 않습니다.";
 		}
 		
-		articles.remove(article);
+		deleteArticle(id);
+		
 		return id+"번 게시물이 삭제되었습니다.";
 	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id+"번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id,title,body);
+		return id+"번 게시물이 수정되었습니다.";
+	}
+
 }
