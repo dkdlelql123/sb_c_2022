55c7379106895eb5da66f0cc25419f449644b47a
NamYeJi <dkdlelql123@naver.com>
2022-06-27 15:12:41 +0900

    SQL을 XML 파일로 옮기고, 수정시 입력된 정보만 수정되도록

--- src/main/java/com/nyj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/nyj/exam/demo/repository/ArticleRepository.java
@@ -12,24 +12,17 @@ import org.apache.ibatis.annotations.Update;
 import com.nyj.exam.demo.vo.Article;
 
 @Mapper
-public interface ArticleRepository {  
-	
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title =  #{title},`body` =  #{body}")
+public interface ArticleRepository {   
 	public void writeArticle(@Param("title") String title, @Param("body") String body);  
-
-	@Select("SELECT * FROM article WHERE id = #{id}")
+ 
 	public Article getArticle(@Param("id") int id); 
-	
-	@Select("SELECT * FROM article")
-	public List<Article> getArticles();  
-	
-	@Delete("DELETE FROM article WHERE id = #{id}")
-	public void deleteArticle(@Param("id") int id); 
 	 
-	@Update("UPDATE article SET title = #{title}, `body`= #{body}, updateDate = NOW() WHERE id =  #{id}")
+	public List<Article> getArticles();  
+	 
+	public void deleteArticle(@Param("id") int id); 
+	  
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
-
-	@Select("SELECT LAST_INSERT_ID()")
+ 
 	public int getLastInsertId();
 	
 }
