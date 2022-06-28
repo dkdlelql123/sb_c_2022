07461d1cd895b949e8e7a5817a5e17642f1771ab
NamYeJi <dkdlelql123@naver.com>
2022-06-28 15:40:00 +0900

    Util.empty 함수 도입

--- src/main/java/com/nyj/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/nyj/exam/demo/controller/UsrMemberController.java
@@ -1,13 +1,12 @@
 package com.nyj.exam.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
- 
-import com.nyj.exam.demo.service.MemberService; 
+
+import com.nyj.exam.demo.service.MemberService;
+import com.nyj.exam.demo.util.Ut;
 import com.nyj.exam.demo.vo.Member;
 
 @Controller
@@ -20,6 +19,25 @@ public class UsrMemberController {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String email, String name, String nickname, String phoneNumber) {
+		
+		if(Ut.empty(loginId)){
+			return "아이디(을)를 입력해주세요.";
+		}
+		if(Ut.empty(loginPw)){
+			return "비밀번호(을)를 입력해주세요.";
+		}
+		if(Ut.empty(email)){
+			return "이메일(을)를 입력해주세요.";
+		}
+		if(Ut.empty(name)){
+			return "이름(을)를 입력해주세요.";
+		}
+		if(Ut.empty(nickname)){
+			return "별명(을)를 입력해주세요.";
+		}
+		if(Ut.empty(phoneNumber)){
+			return "핸드폰번호(을)를 입력해주세요.";
+		}
 		
 		int join = memberService.join(loginId, loginPw, email, name, nickname, phoneNumber);
 		if(join == -1) return "이미 존재하는 계정 아이디입니다.";
