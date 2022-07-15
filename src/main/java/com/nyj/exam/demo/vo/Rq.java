942eb80fbc18a9c7cd96e55109445f1da9c1d3dd
NamYeJi <dkdlelql123@naver.com>
2022-07-16 05:44:23 +0900

    필요할때만 rq 생성하도록 변경

--- src/main/java/com/nyj/exam/demo/vo/Rq.java
+++ src/main/java/com/nyj/exam/demo/vo/Rq.java
@@ -102,13 +102,6 @@ public class Rq {
 	public String getEncodedCurrentUri(){
 		return Ut.getUriEncoded(getCurrentUri());
 	}
-	
-	// 이 메서드는 Rq 객체가 자연스럽게 생성되도록 유도하는 역할을 한다.
-	// 지우면 안되고,
-	// 편의를 위해 BeforeActionInterceptor 에서 꼭 호출해줘야 한다.
-	public void initOnBeforeActionInterceptor() {
-		
-	}
 
 	public String historyBackJsOnView(String msg) {
 		req.setAttribute("msg", msg);
